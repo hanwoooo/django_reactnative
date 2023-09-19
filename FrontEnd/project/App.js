4d8903bb84f1d7d0ae8282a59671468176b23ee6
hanwoooo <dngur515@naver.com>
2023-09-19 22:53:48 +0900

    test

--- FrontEnd/project/App.js
+++ FrontEnd/project/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello~ I'm Han Woohyeok</Text>
+      <Text>Hello~ I'm Han Woohyeok~~~</Text>
       <StatusBar style="auto" />
     </View>
   );
