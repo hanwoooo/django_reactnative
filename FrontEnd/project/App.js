a1fa384b0b443d2d7f0ff9d0473f7986e61102e6
hanwoooo <dngur515@naver.com>
2023-09-21 21:00:25 +0900

    23.09.21 add button event

--- FrontEnd/project/App.js
+++ FrontEnd/project/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity, } from 'react-native';
 
 export default function App() {
   return (
@@ -8,9 +9,16 @@ export default function App() {
       <Text style={styles.text}>
         Click Start!!
       </Text>
-      <Text style={styles.buttontext}>
-        Start
-      </Text>
+      <View style={styles.button}>
+        <TouchableOpacity
+          onPress={Alert.alert("Hello~~")}
+        >
+          {/* 커스텀 버튼을 만들기 위해 사용*/}
+          <Text style={styles.buttonText}>
+            Start
+          </Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,32 +29,27 @@ const styles = StyleSheet.create({ //자동완성이 필요할 경우 무조건 
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    marginHorizontal: 16
     //justifyContent: 'center',
   },
   text: {
     marginTop: 160,
     fontSize: 40,
   },
-  startbutton: {
+  button: {
     marginTop: 240,
-    justifyContent: 'center',
-    backgroundColor: '#1E90FF',
-    width: 200,
-    height: 100,
-    borderRadius: 20,
-    borderTopRightRadius: 80,
-  },
-  buttontext: {
-    color: "white",
-    fontSize: 75,
-    marginTop: 240,
-    backgroundColor: '#1E90FF',
+    backgroundColor: "#1E90FF",
     alignSelf: 'center',
     justifyContent: 'center',
     width: 200,
     height: 100,
-    borderRadius: 20,
+    borderRadius: 30,
     borderTopRightRadius: 80,
+  },
+  buttonText: {
+    marginLeft: 20,
+    color: "white",
+    fontSize: 75,
   }
 
 });
